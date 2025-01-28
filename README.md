78b226e9d72b7d5e9744407683fd13486e43247a
Leon Joyce <ljoyce2@sheffield.ac.uk>
2025-01-28 16:08:33 +0000

    simplified logic

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Bolus Calculator https://leonjoyce88.github.io/BolusCalc/
 
-This calculator is to aid people with diabetes in dosing descisions for their mealtime insulin.
+This calculator is to aid diabetics with dosing descisions for their mealtime insulin.
 
 # Features
-Fetches my current BG data
+Fetches my current BG data(hope to extend this in future)
 Allows input of parameters to calculate bolus dosage for mealtime/correction insulin
+
+
+
+
 
--- src/App.jsx
+++ src/App.jsx
@@ -7,53 +7,47 @@ import { DexcomClient } from 'dexcom-share-api';
 
 
 function App() {
-    const [mmol, setMmol] = useState(6)
-    const [time, setTime] = useState('')
-    const [trend, setTrend] = useState('')
+    const [reading, setReading] = useState({ mmol: 6 })
     const [ratio, setRatio] = useState(40)
     const [factor, setFactor] = useState(6)
     const [target, setTarget] = useState(6)
     const [carbs, setCarbs] = useState(0)
-    const [bolus, setBolus] = useState('')
+    const bolus = () => {
+        let correction = (reading.mmol - target) / factor
+        let meal = carbs / ratio
+        let total = Math.trunc(2 * (correction + meal)) / 2;
 
+        if (total < 0.5 || isNaN(total)) {
+            return (0)
+        } else {
+            return (total)
+        }
+
+    }
     useEffect(() => {
-        if (time == '' || (Date.now() - time) >= 300000) {
+        if (!reading.time || (reading && (Date.now() - reading.timestamp) >= 300000)) {
             fetch('https://boluscalc-production.up.railway.app')
                 .then(res => res.json())
                 .then(data => {
-                    setMmol(data[0].mmol)
-                    setTime(data[0].timestamp)
-                    setTrend(data[0].trend)
+                    setReading(data[0])
                 })
         }
-        calculateBolus()
-    }, [mmol, ratio, factor, target, carbs, bolus])
+    }, [ratio, factor, target, carbs])
 
-    const calculateBolus = () => {
-        let correction = (mmol - target) / factor
-        let meal = carbs / ratio
-        let total = Math.trunc(2 * (correction + meal)) / 2;
-        console.log(total)
 
-        if (total < 0.5) {
-            setBolus(0)
-        } else {
-            setBolus(total)
-        }
-    }
     return (
         <>
             <div className="top-info">
                 <div className="form-row">
                     <label htmlFor="blood-glucose">Current Blood Glucose:</label>
-                    <input id="blood-glucose" type="number" value={mmol} onChange={e => setMmol(e.target.value)} />
+                    <input id="blood-glucose" type="number" value={reading ? reading.mmol : "6"} onChange={e => setReading({ ...reading, mmol: e.target.value })} />
                 </div>
 
                 <div className="trend-time-row">
                     <span className="trend-label">Trend:</span>
-                    <span>{trend ? trend : "Waiting for Data"}</span>
+                    <span>{reading.trend ? reading.trend : "Waiting for Data"}</span>
                     <span className="time-label">Time:</span>
-                    <span>{time ? new Date(time).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' }) : 'Waiting for Data'}</span>
+                    <span>{reading.timestamp ? new Date(reading.timestamp).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' }) : 'Waiting for Data'}</span>
                 </div>
             </div>
 
@@ -80,7 +74,8 @@ function App() {
             </div>
 
             <div className="bolus-result">
-                <h1>{bolus ? (bolus + " units") : "None"}</h1>
+                {/*<h1>{bolus ? (bolus + " units") : "None"}</h1>*/}
+                <h1>{bolus() + " units"}</h1>
             </div>
         </>
     );
